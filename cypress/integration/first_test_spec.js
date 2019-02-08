f40645e804e74b6744eb783b995e8fcac59e7410
anodal <anodal@ncl.com>
2019-02-08 14:45:40 -0500

    change color of btn border

--- cypress/integration/first_test_spec.js
+++ cypress/integration/first_test_spec.js
@@ -1,25 +1,33 @@
-describe('first test',function(){
-    it("Does not much",function(){
-        var state={
-            index:"http://localhost:10800/percy-poc/index.html",
-            checkoutPg:'checkout.html',
-            contactPg:'contact.html',
-            clientFirstName:"Antonio",
-            clientLastName:"Nodal",
-            contactUsText:"Contact Us"
-        }        
-        //- visits the home page
-        cy.visit(state.index)
-        //- click on a link
-        cy.contains('Checkout').click()
-        //- do a test assertion about the content
-        cy.url().should('include',state.checkoutPg)
-        //- fill first and Last name
-        cy.get('#firstName')
-            .type(state.clientFirstName)
-            .should('have.value',state.clientFirstName)
-        cy.contains(state.contactUsText).click()  
-        cy.url().should('include',state.contactPg)
-        cy.contains('Home').click()
+
+describe('@percy/cypress', function() {
+    describe('first test',function(){
+        it("Does not much",function(){
+            var state={
+                index:"http://localhost:10800/percy-poc/pages/index.html",
+                checkoutPg:'checkout.html',
+                contactPg:'contact.html',
+                clientFirstName:"Antonio",
+                clientLastName:"Nodal",
+                contactUsText:"Contact Us"
+            }        
+            //- visits the home page
+            cy.visit(state.index)
+            //take an snapshot
+            //cy.screenshot()
+            // Take a snapshot for visual diffing
+            cy.percySnapshot()
+            console.log("here")    
+            //- click on a link
+            cy.contains('Checkout').click()
+            //- do a test assertion about the content
+            cy.url().should('include',state.checkoutPg)
+            //- fill first and Last name
+            cy.get('#firstName')
+                .type(state.clientFirstName)
+                .should('have.value',state.clientFirstName)
+            cy.contains(state.contactUsText).click()  
+            cy.url().should('include',state.contactPg)
+            cy.contains('Home').click()
+        })
     })
 })
